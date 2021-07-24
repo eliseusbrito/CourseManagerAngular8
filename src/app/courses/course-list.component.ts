06c26c232b4fe4e015e4640ed44fa16b71cd87ec
eliseusbrito <eliseusbrito@gmail.com>
2021-07-24 10:48:00 -0300

    Adiciona course.service.ts para injetar como dependencia em course-list.component

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './course.service';
 import { HtmlParser } from "@angular/compiler";
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
@@ -10,29 +11,9 @@ export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
 
-    ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: "XPS-8796",
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'November, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: "LKL-1094",
-                duration: 80,
-                rating: 4,
-                releaseDate: 'November, 4, 2019'
-            }
-        ]
-    }
+    constructor(private courseService: CourseService) { }
 
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
+    }
 }
