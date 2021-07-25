6d6c18398a3231c56c02d0aec627569ced887075
eliseusbrito <eliseusbrito@gmail.com>
2021-07-25 09:53:26 -0300

    Adiciona edicao dos cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
