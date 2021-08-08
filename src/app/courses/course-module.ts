58a1e79327696720391b831bfde2ca1e15bf32bd
eliseusbrito <eliseusbrito@gmail.com>
2021-08-08 08:32:36 -0300

    Segregado Replace pipe com criacao do app-pipe.module.ts

--- src/app/courses/course-module.ts
+++ src/app/courses/course-module.ts
@@ -1,5 +1,3 @@
-import { StarComponent } from '../shared/component/star/star-component';
-import { ReplacePipe } from './../pipe/replace.pipe';
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
@@ -7,17 +5,18 @@ import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 import { CommonModule } from '@angular/common';
 import { StarModule } from '../shared/component/star/star.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 
 @NgModule({
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
     ],
     imports: [
         CommonModule,
         FormsModule,
         StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
